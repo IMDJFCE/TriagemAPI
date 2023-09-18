61be7f24b1e2dc6341a3b2877c874ecbb26ca6ac
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2023-09-18 11:19:58 -0300

    adicionando os metodos getbyid, put e delete no controller de oportunidade

--- src/main/java/br/com/jfce/apibancotalentos/controller/OportunidadeController.java
+++ src/main/java/br/com/jfce/apibancotalentos/controller/OportunidadeController.java
@@ -1,6 +1,7 @@
 package br.com.jfce.apibancotalentos.controller;
 
 import br.com.jfce.apibancotalentos.model.Oportunidade;
+import br.com.jfce.apibancotalentos.model.Perfil;
 import br.com.jfce.apibancotalentos.service.OportunidadeService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("oportunidades")
+@RequestMapping("/api/oportunidades/")
 @AllArgsConstructor
 public class OportunidadeController {
     private OportunidadeService service;
@@ -18,8 +19,23 @@ public class OportunidadeController {
         return service.findAll();
     }
 
+    @GetMapping("{id}")
+    public Oportunidade getById(@PathVariable String id){
+        return service.getById(id);
+    }
+
     @PostMapping
     public void create(@RequestBody Oportunidade oportunidade){
         service.create(oportunidade);
+    }
+
+    @PutMapping
+    public Oportunidade update(@RequestBody Oportunidade oportunidade){
+        return service.update(oportunidade);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        service.delete(id);
     }
 }
