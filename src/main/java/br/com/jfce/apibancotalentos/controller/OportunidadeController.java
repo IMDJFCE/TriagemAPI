eb0719c12129a891880281187464f1e40251dad0
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2023-09-19 17:07:01 -0300

    documentando o perfil controller e oportunidade controller com o swagger

--- src/main/java/br/com/jfce/apibancotalentos/controller/OportunidadeController.java
+++ src/main/java/br/com/jfce/apibancotalentos/controller/OportunidadeController.java
@@ -2,6 +2,8 @@ package br.com.jfce.apibancotalentos.controller;
 
 import br.com.jfce.apibancotalentos.model.Oportunidade;
 import br.com.jfce.apibancotalentos.service.OportunidadeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,30 +12,54 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/oportunidades/")
 @AllArgsConstructor
+@Tag(
+        name = "Oportunidade Controller",
+        description = "Controlador responsável por gerenciar operações relacionadas a oportunidades."
+)
 public class OportunidadeController {
     private OportunidadeService service;
 
     @GetMapping
+    @Operation(
+            summary = "Listar Todas as Oportunidades",
+            description = "Este endpoint retorna uma lista de todas as oportunidades disponíveis."
+    )
     public List<Oportunidade> listAll(){
         return service.findAll();
     }
 
     @GetMapping("{id}")
+    @Operation(
+            summary = "Obter uma Oportunidade por ID",
+            description = "Este endpoint retorna uma oportunidade específica com base no ID fornecido."
+    )
     public Oportunidade getById(@PathVariable String id){
         return service.getById(id);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Criar uma Nova Oportunidade",
+            description = "Este endpoint permite criar uma nova oportunidade."
+    )
     public void create(@RequestBody Oportunidade oportunidade){
         service.create(oportunidade);
     }
 
     @PutMapping
+    @Operation(
+            summary = "Atualizar uma Oportunidade Existente",
+            description = "Este endpoint permite atualizar os dados de uma oportunidade existente."
+    )
     public Oportunidade update(@RequestBody Oportunidade oportunidade){
         return service.update(oportunidade);
     }
 
     @DeleteMapping("{id}")
+    @Operation(
+            summary = "Excluir uma Oportunidade",
+            description = "Este endpoint permite excluir uma oportunidade com base no ID fornecido."
+    )
     public void delete(@PathVariable String id){
         service.delete(id);
     }
--- src/main/java/br/com/jfce/apibancotalentos/controller/PerfilController.java
+++ src/main/java/br/com/jfce/apibancotalentos/controller/PerfilController.java
@@ -2,6 +2,8 @@ package br.com.jfce.apibancotalentos.controller;
 
 import br.com.jfce.apibancotalentos.model.Perfil;
 import br.com.jfce.apibancotalentos.service.PerfilService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,30 +12,54 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/perfis/")
 @AllArgsConstructor
+@Tag(
+        name = "Perfil Controller",
+        description = "Controlador responsável por gerenciar operações relacionadas a perfis."
+)
 public class PerfilController {
     private PerfilService service;
 
     @GetMapping
+    @Operation(
+            summary = "Listar Todos os Perfis",
+            description = "Este endpoint retorna uma lista de todos os perfis disponíveis"
+    )
     public List<Perfil> listAll(){
         return service.findAll();
     }
 
     @GetMapping("{id}")
+    @Operation(
+            summary = "Obter um Perfil por ID",
+            description = "Este endpoint retorna um perfil específico com base no ID fornecido."
+    )
     public Perfil getById(@PathVariable String id){
         return service.getById(id);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Criar um Novo Perfil",
+            description = "Este endpoint permite criar um novo perfil."
+    )
     public void create(@RequestBody Perfil perfil){
         service.create(perfil);
     }
 
     @PutMapping
+    @Operation(
+            summary = "Atualizar um Perfil Existente",
+            description = "Este endpoint permite atualizar os dados de um perfil existente."
+    )
     public Perfil update(@RequestBody Perfil perfil){
         return service.update(perfil);
     }
 
     @DeleteMapping("{id}")
+    @Operation(
+            summary = "Excluir um Perfil",
+            description = "Este endpoint permite excluir um perfil com base no ID fornecido."
+    )
     public void delete(@PathVariable String id){
         service.delete(id);
     }
