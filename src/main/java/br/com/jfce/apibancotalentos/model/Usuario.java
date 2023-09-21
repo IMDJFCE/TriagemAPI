4aba1eb4ef4fff60d3d0c2a7e5b31ca122eb0a5e
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2023-09-21 16:35:47 -0300

    adicionando validacoes na classe usuario

--- src/main/java/br/com/jfce/apibancotalentos/model/Usuario.java
+++ src/main/java/br/com/jfce/apibancotalentos/model/Usuario.java
@@ -1,12 +1,16 @@
 package br.com.jfce.apibancotalentos.model;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
 @AllArgsConstructor
@@ -14,11 +18,29 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class Usuario extends AbstractEntity<Usuario>{
+
+    @NotBlank(message = "O campo nome não pode estar em branco.")
+    @Size(max = 100, message = "O campo nome deve ter no máximo 100 caracteres.")
     private String nome;
+
+    @NotBlank(message = "O campo email não pode estar em branco.")
+    @Email(message = "O e-mail deve ser um endereço de e-mail válido.")
+    @Size(max = 70, message = "O campo e-mail deve ter no máximo 70 caracteres.")
     private String email;
+
+    @NotBlank(message = "O campo senha não pode estar em branco.")
+    @Size(min = 8, max = 50, message = "O campo senha deve ter no mínimo 8 caracteres e no máximo 50 caracteres.")
     private String senha;
-    private LocalDateTime dataNascimento;
+
+    @NotNull(message = "O campo data de nascimento não pode ser nulo.")
+    private LocalDate dataNascimento;
+
+    @NotBlank(message = "O campo matrícula não pode estar em branco.")
+    @Size(max = 50, message = "O campo matrícula deve ter no máximo 50 caracteres.")
     private String matricula;
+
+    @NotBlank(message = "O campo vínculo não pode estar em branco.")
+    @Size(max = 15, message = "O campo vínculo deve ter no máximo 15 caracteres.")
     private String vinculo;
 
 
