a3cd8e36029c08fd1aa8f3289ac3535be07ac0e5
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2023-09-22 10:22:22 -0300

    ajustando classe abstrata

--- src/main/java/br/com/jfce/apibancotalentos/model/AbstractEntity.java
+++ src/main/java/br/com/jfce/apibancotalentos/model/AbstractEntity.java
@@ -1,24 +1,17 @@
 package br.com.jfce.apibancotalentos.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public abstract class AbstractEntity<E> implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+public abstract class AbstractEntity{
 
     @CreationTimestamp
     private LocalDateTime createdAt;
@@ -28,7 +21,4 @@ public abstract class AbstractEntity<E> implements Serializable {
 
 
     private LocalDateTime deletedAt;
-
-    public abstract void update(E source);
-    public abstract E createNewInstance();
 }
