cca0420193c3e985ce3ef27b230bec1f601cf6c6
David  Willian Pereira  Jatoba <r_david.jatoba@jfce.jus.br>
2023-11-17 11:53:00 -0300

    removendo paginacao do repository de habilidade

--- src/main/java/br/com/jfce/apibancotalentos/repository/HabilidadeRepository.java
+++ src/main/java/br/com/jfce/apibancotalentos/repository/HabilidadeRepository.java
@@ -2,10 +2,9 @@ package br.com.jfce.apibancotalentos.repository;
 
 import br.com.jfce.apibancotalentos.model.Habilidade;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface HabilidadeRepository extends JpaRepository<Habilidade, Long>, PagingAndSortingRepository<Habilidade, Long> {
+public interface HabilidadeRepository extends JpaRepository<Habilidade, Long> {
     Optional<Habilidade> findByNome(String nome);
 }
